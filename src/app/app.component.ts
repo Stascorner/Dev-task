a48fafceecb5b264985a932b17dc52553b449b29
Stas Kutasevych <stascorner@gmail.com>
2020-09-08 12:04:22 +0300

    Add ngx-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpService} from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +7,11 @@ import { HttpService} from './http.service';
 })
 export class AppComponent {
   
-  books: any[]=[];
-     
-    constructor(private httpService: HttpService){}
+  
       
     ngOnInit(){
           
-        this.httpService.getData().subscribe(data => this.books=data["bookList"]);
+        
     }
 
 }
