af4520298ea2009c4a384cfeac466b48acbaad97
Stas Kutasevych <stascorner@gmail.com>
2020-09-08 10:55:08 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService} from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dev-test';
+  
+  books: any[]=[];
+     
+    constructor(private httpService: HttpService){}
+      
+    ngOnInit(){
+          
+        this.httpService.getData().subscribe(data => this.books=data["bookList"]);
+    }
+
 }
